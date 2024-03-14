7c71cdb181fede092cf9d885805fa78ec806cd88
Romraev <romraev@yandex.ru>
2024-03-14 18:50:53 +0100

    new commit

--- javascript/oojs/bouncing-balls/main-finished.js
+++ javascript/oojs/bouncing-balls/main-finished.js
@@ -5,6 +5,7 @@ const ctx = canvas.getContext('2d');
 
 const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight;
+const counter = document.querySelector('p')
 
 // function to generate random number
 
@@ -18,13 +19,20 @@ function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
 
-class Ball {
-
-   constructor(x, y, velX, velY, color, size) {
+class Shape {
+   constructor(x, y, velX, velY, exists) {
       this.x = x;
       this.y = y;
       this.velX = velX;
       this.velY = velY;
+      this.exists = exists
+   }
+}
+
+class Ball extends Shape {
+
+   constructor(x, y, velX, velY, exists, color, size) {
+      super(x, y, velX, velY, exists)
       this.color = color;
       this.size = size;
    }
@@ -59,18 +67,83 @@ class Ball {
 
    collisionDetect() {
       for (const ball of balls) {
-         if (!(this === ball)) {
+         if (!(this === ball) && ball.exists) {
             const dx = this.x - ball.x;
             const dy = this.y - ball.y;
             const distance = Math.sqrt(dx * dx + dy * dy);
 
             if (distance < this.size + ball.size) {
               ball.color = this.color = randomRGB();
+              this.velX = -this.velX;
+               this.velY = -this.velY;
+               ball.velX = -ball.velX;
+               ball.velY = -ball.velY;
             }
          }
       }
    }
 
+}
+
+class EvilCircle extends Shape {
+   constructor(x, y, exists) {
+      super(x, y, 20, 20, exists)
+      this.color = 'white'
+      this.size = 10
+   }
+   draw() {
+      ctx.beginPath();
+      ctx.lineWidth = 4;
+      ctx.strokeStyle = this.color;
+      ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+      ctx.stroke();
+   }
+   checkBounds() {
+      if ((this.x + this.size) >= width) {
+         this.x -= this.size;
+      }
+
+      if ((this.x - this.size) <= 0) {
+         this.x += this.size;
+      }
+
+      if ((this.y + this.size) >= height) {
+         this.y -= this.size;
+      }
+
+      if ((this.y - this.size) <= 0) {
+         this.y += this.size;
+      }
+   }
+   setControls() {
+      var _this = this;
+      window.onkeydown = function (e) {
+         if (e.code === "ArrowLeft") {
+           _this.x -= _this.velX;
+         } else if (e.code === "ArrowRight") {
+           _this.x += _this.velX;
+         } else if (e.code === "ArrowUp") {
+           _this.y -= _this.velY;
+         } else if (e.code === "ArrowDown") {
+           _this.y += _this.velY;
+         }
+      };
+   }
+   collisionDetect() {
+      for (const ball of balls) {
+         if (ball.exists) {
+            const dx = this.x - ball.x;
+            const dy = this.y - ball.y;
+            const distance = Math.sqrt(dx * dx + dy * dy);
+
+            if (distance < this.size + ball.size) {
+              ball.exists = false;
+              return 1
+            }
+         }
+      }
+      return 0
+   }
 }
 
 const balls = [];
@@ -84,24 +157,40 @@ while (balls.length < 25) {
       random(0 + size,height - size),
       random(-7,7),
       random(-7,7),
+      true,
       randomRGB(),
       size
    );
-
   balls.push(ball);
 }
+
+const evil = new EvilCircle(
+   random(10,width - 10),
+   random(10,height - 10),
+   true
+)
 
 function loop() {
    ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
    ctx.fillRect(0, 0,  width, height);
 
    for (const ball of balls) {
-     ball.draw();
-     ball.update();
-     ball.collisionDetect();
+      if(ball.exists) {
+         ball.draw();
+         ball.update();
+         ball.collisionDetect();
+      }
    }
+   evil.draw()
+   evil.checkBounds()
+   count -= evil.collisionDetect()
+   counter.innerText = `Balls count: ${count}`
 
    requestAnimationFrame(loop);
 }
+
+evil.setControls()
+
+var count = balls.length
 
 loop();
--- javascript/oojs/bouncing-balls/main.js
+++ javascript/oojs/bouncing-balls/main.js
@@ -18,3 +18,102 @@ function random(min, max) {
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
+
+function Shape(x, y, velX, velY) {
+  this.x = x; //posición horizontal
+  this.y = y; //posición vertical
+  this.velX = velX; //velocidad horizontal
+  this.velY = velY; //velocidad vertical
+  // this.color = color; //color
+  // this.size = size; //tamaño
+}
+
+function Ball(x, y, velX, velY, color, size) {
+  Shape.call(this, x, y, velX, velY)
+  this.color = color; //color
+  this.size = size; //tamaño
+}
+
+Ball.prototype = Object.create(Shape.prototype)
+
+Ball.prototype.draw = function () {
+  ctx.beginPath();
+  ctx.fillStyle = this.color;
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+  ctx.fill();
+};
+
+Ball.prototype.update = function () {
+  if (this.x + this.size >= width) {
+    this.velX = -this.velX;
+  }
+
+  if (this.x - this.size <= 0) {
+    this.velX = -this.velX;
+  }
+
+  if (this.y + this.size >= height) {
+    this.velY = -this.velY;
+  }
+
+  if (this.y - this.size <= 0) {
+    this.velY = -this.velY;
+  }
+
+  this.x += this.velX;
+  this.y += this.velY;
+};
+
+Ball.prototype.collisionDetect = function () {
+  for (var j = 0; j < balls.length; j++) {
+    if (!(this === balls[j])) {
+      var dx = this.x - balls[j].x;
+      var dy = this.y - balls[j].y;
+      var distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < this.size + balls[j].size) {
+        balls[j].color = this.color = `rgb(${random(0, 255)},${random(
+          0,
+          255,
+        )},${random(0, 255)})`;
+        this.velX = -this.velX;
+        this.velY = -this.velY;
+        balls[j].velX = -balls[j].velX;
+        balls[j].velY = -balls[j].velY;
+      }
+    }
+  }
+};
+
+var balls = [];
+
+function loop() {
+  ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
+  ctx.fillRect(0, 0, width, height);
+
+  while (balls.length < 25) {
+    var size = random(10, 20);
+    var ball = new Ball(
+      // la posición de las pelotas, se dibujará al menos siempre
+      // como mínimo a un ancho de la pelota de distancia al borde del
+      // canvas, para evitar errores en el dibujo
+      random(0 + size, width - size),
+      random(0 + size, height - size),
+      random(-7, 7),
+      random(-7, 7),
+      `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`,
+      size,
+    );
+    balls.push(ball);
+  }
+
+  for (var i = 0; i < balls.length; i++) {
+    balls[i].draw();
+    balls[i].update();
+    balls[i].collisionDetect();
+  }
+
+  requestAnimationFrame(loop);
+}
+
+loop()
